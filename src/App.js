af267a872b3f6223e80f98e8e919c42b450a79af
eglalita <eglalita@gmail.com>
2017-09-24 09:58:23 +0700

    Basic Route Pages

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ItemList from './ItemList'
-import AddItem from './AddItem'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      item: [],
-    }
-  }
-  addItem(text){
-    this.setState({
-      item: [...this.state.item, text]
-    })
-  }
-
-removeItem(itemindex){
-  this.setState({
-  item: this.state.item.filter((value,index)=>{
-    if(index===itemindex){
-      return false
-    }
-    else{
-      return true
-    }
-  })
-})
-}
-
-  // filterItem(text){
-  //   this.setState({
-
-  //   })
-  // }
-
   render() {
-    const {item} = this.state
     return (
-       <div className = "app">
-          <ItemList item= {item} handleRemove={(itemIndex)=>this.removeItem(itemIndex)} />
-          <AddItem onClick={(text)=>this.addItem(text)}/>
-       </div>
+       <BrowserRouter>
+          <div className="app">
+              <Switch>
+                <Route exact path="/" component={Temp}/>
+                <Route exact path="/a" component={Temp2}/>
+                <Route exact path="/a/b" component={Temp3}/>
+              </Switch>
+          </div>
+       </BrowserRouter>
     )
   }
+}
+
+const Temp = () => {
+    return <div>TEMP COMPONENT</div>
+ }
+ const Temp2 = () => {
+  return <div>TEMP COMPONENT2</div>
+}
+const Temp3 = () => {
+  return <div>TEMP COMPONENT3</div>
 }
 
 export default App;
