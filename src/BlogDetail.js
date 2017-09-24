913fe53432cbda47ffa84b920b1187e6b7de4654
eglalita <eglalita@gmail.com>
2017-09-24 10:45:52 +0700

    binding parameter in BlogDetail

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -8,9 +8,15 @@ class BlogDetail extends Component {
  render() {
     //  console.log(this.props)
     //const {match} = this.props
-    setTimeout(()=>{
-        this.props.history.push('/')
-    }, 3000)
+    // setTimeout(()=>{
+    //     this.props.history.push('/')
+    // }, 3000)
+    const {match} =  this.props
+    const {params} = match
+    const {id} = params
+
+    const {match: {params: {id}}} = this.props //similar with previous 3 lines
+
    return (
        <div className="blog-detail">
            <div>
